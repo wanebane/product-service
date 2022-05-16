8440966de14ec61943b386f8688bf4414498b446
Romy Rivaldy <romy.rivaldy@gmail.com>
2022-05-16 14:49:10 +0700

    Initial Commit

---
+++ product-service/src/main/java/com/rivaldy/ecom/entity/TblProduct.java
@@ -0,0 +1,13 @@
+package com.rivaldy.ecom.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "tbl_product")
+@Data
+public class TblProduct {
+
+}
---
+++ product-service/src/test/java/com/rivaldy/ecom/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rivaldy.ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
